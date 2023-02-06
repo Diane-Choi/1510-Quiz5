d10b9a9177830bee481fba9e68e3c16d2e5a31eb
Sim Amy <amy.nr.sim@gmail.com>
2023-02-06 11:32:31 -0800

    Modify blank spaces

--- test_calculate_pay.py
+++ test_calculate_pay.py
@@ -2,7 +2,9 @@ from unittest import TestCase
 
 from calculate_pay import calculate_pay
 
+
 class TestCalculatePay(TestCase):
+
     def test_zero_pay_wage(self):
         self.assertEqual(0, calculate_pay(0, 0))
 
@@ -16,16 +18,16 @@ class TestCalculatePay(TestCase):
         self.assertEqual(0, calculate_pay(99, -99))
 
     def test_zero_wage_positive_hour(self):
-         self.assertEqual(0, calculate_pay(99, 0))
+        self.assertEqual(0, calculate_pay(99, 0))
 
     def test_zero_wage_negative_hour(self):
         self.assertEqual(0, calculate_pay(-99, 0))
 
     def test_positive_wage_positive_hour(self):
-         self.assertEqual(2000000, calculate_pay(1000, 1000))
+        self.assertEqual(2000000, calculate_pay(1000, 1000))
 
     def test_positive_wage_zero_hour(self):
-         self.assertEqual(0, calculate_pay(0, 99))
+        self.assertEqual(0, calculate_pay(0, 99))
 
     def test_positive_wage_negative_hour(self):
-         self.assertEqual(0, calculate_pay(-99, 1000))
+        self.assertEqual(0, calculate_pay(-99, 1000))
