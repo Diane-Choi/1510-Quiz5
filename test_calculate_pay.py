671fd0cce7746917e71cfb9906dfbcf4c0f1f7e3
Sim Amy <amy.nr.sim@gmail.com>
2023-02-06 11:24:42 -0800

    Update unit test file

--- test_calculate_pay.py
+++ test_calculate_pay.py
@@ -6,8 +6,26 @@ class TestCalculatePay(TestCase):
     def test_zero_pay_wage(self):
         self.assertEqual(0, calculate_pay(0, 0))
 
-    def test_calculate_pay(self):
-        self.assertEqual(0, calculate_pay(0, 0))
+    def test_negative_pay_wage(self):
+        self.assertEqual(0, calculate_pay(-99, -99))
 
-    def test_calculate_pay(self):
-        self.assertEqual(0, calculate_pay(0, 0))
+    def test_negative_wage_zero_hour(self):
+        self.assertEqual(0, calculate_pay(0, -99))
+
+    def test_negative_wage_positive_hour(self):
+        self.assertEqual(0, calculate_pay(99, -99))
+
+    def test_zero_wage_positive_hour(self):
+         self.assertEqual(0, calculate_pay(99, 0))
+
+    def test_zero_wage_negative_hour(self):
+        self.assertEqual(0, calculate_pay(-99, 0))
+
+    def test_positive_wage_positive_hour(self):
+         self.assertEqual(2000000, calculate_pay(1000, 1000))
+
+    def test_positive_wage_zero_hour(self):
+         self.assertEqual(0, calculate_pay(0, 99))
+
+    def test_negative_wage_positive_hour(self):
+         self.assertEqual(0, calculate_pay(-99, 1000))
